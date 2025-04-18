9a07b4954ffd8d3a2b99c5d9185319dfab966d47
Yunus-Web-Dev <saidahmedovunus62@gmail.com>
2025-04-18 05:22:23 +0500

    Избавился от скрола при открытие модалки

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ document.querySelectorAll('.diploms_container img').forEach(img => {
     img.addEventListener('click', ()=> {
         popup.classList.remove('hidden');
         popupImg.src = img.src; //Можно заменить на увеличенную версию, если нужно
+        document.body.style.overflow = 'hidden'; // 💡 Блокируем скролл
     });
 });
 
 function closePopup() {
     popup.classList.add('hidden');
     popupImg.src= '';
+    document.body.style.overflow = ''; // 💡 Возвращаем прокрутку
 }
